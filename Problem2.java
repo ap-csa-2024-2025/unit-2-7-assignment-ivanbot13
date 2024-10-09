54e4a6b48a4ef7f580e8cd6dfdcbf2e118f8f71b
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-10-09 10:56:16 -0700

    1 and 2 done

--- Problem2.java
+++ Problem2.java
@@ -8,12 +8,12 @@ public class Problem2
     Integer x;
     Integer y;
 
-    System.out.println(x + y);
     System.out.println("Enter values:");
-    x = scan.nextLong();
+    x = scan.nextInt();
     y = scan.nextInt();
+    System.out.println(x + y);
 
-    Double avg = (double)(x + y / 2);
+    Double avg = (((double)x + y) / 2);
     System.out.println("Average of " + x + " and " + y + " is " + avg);
   }
 }
